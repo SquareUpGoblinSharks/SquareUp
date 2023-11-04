5c62e4c9add73d151bd2c961ccc7ebf96827cb2a
Jerry Trinh <jtjerrytrinh@gmail.com>
2023-11-04 11:52:51 -0700

    test

---
+++ servers/controllers.js
@@ -0,0 +1,10 @@
+const User = require('./models');
+const bcrypt = require('bcryptjs');
+
+const Controller = {};
+
+Controller.createUser = (req, res, next)
+
+Controller.verifyUser = (req, res, next) => {
+    
+}
