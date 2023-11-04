8675f2aea19c962c45c15536c4f8d24f8f253faf
Brian Lee <bnlee419@gmail.com>
2023-11-04 14:24:50 -0700

    basic layour

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Test from './Test.jsx';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Test />} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        {/* <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} /> */}
         {/* Route for after you select "Square Up" on someone */}
-        <Route path="/squareup" component={SquareUp} />
+        {/* <Route path="/squareup" component={SquareUp} /> */}
       </Routes>
     </Router>
   );
