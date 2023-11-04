14ab9a01aad4b2622f9329ef27e69af7dde7492b
keidy9 <keidyw999@gmail.com>
2023-11-04 11:23:43 -0700

    fixed index.html script src, added test route to ensure react router is working properly

--- client/App.jsx
+++ client/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Test from './Test';
 
 const App = () => {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route path="/" component={HomePage} />
-          <Route path="/login" component={Login} />
+      <Routes>
+        <Route path="/" element={<Test />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        {/* <Route path="/login" component={Login} />
           <Route path="/signup" component={Signup} />
-          <Route path="/squareup" component={SquareUp} />
-          {/* Route for after you select "Square Up" on someone */}
-        </Switch>
-      </div>
+        <Route path="/squareup" component={SquareUp} /> */}
+        {/* Route for after you select "Square Up" on someone */}
+      </Routes>
     </Router>
   );
 };
