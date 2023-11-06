d68e2092cc1b15779c1179d2d3cedce4486efbb1
William Kil <william.b.kil@gmail.com>
2023-11-06 10:43:15 -0800

    homepage

---
+++ client/HomePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Profile from './Profile.jsx';
+import Leaderboard from './Leaderboard.jsx';
+import UpcomingMatches from './UpcomingMatches';
+import ButtonBar from './ButtonBar.jsx';
+
+const HomePage = () => {
+  return (
+      <div className="mainContainer">
+        <div className="leftContainer">
+          <Leaderboard/>
+        </div>
+        <div className="middleContainer">
+          <Profile/>
+          <ButtonBar/>
+        </div>
+        <div className="rightContainer">
+          <UpcomingMatches/>
+        </div>
+      </div>
+  );
+};
+
+export default HomePage;
