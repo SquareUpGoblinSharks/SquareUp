cfd34ec7266fee342b9a40a968549965548f13d9
John Dunn <johnwdunn20@gmail.com>
2023-11-08 14:37:31 -0800

    Styled buttons

--- client/routes/Prototype.jsx
+++ client/routes/Prototype.jsx
@@ -4,17 +4,20 @@ import HexGreenBGWrapper from '../components/HexGreenBGWrapper'
 import Button from '../components/Button';
 
 const Prototype = props => {
-
+  /*
+  Colors from: https://coolors.co/395e66-387d7a-32936f-26a96c-2bc016
+    #2BC016
+    #26A96C
+    #32936F
+    #387D7A
+    #395E66
+  */
   return (
-    <>
-    {/* <HexGreenBGWrapper>
-      <CenteredWrapper> 
-        Hello!
-      </CenteredWrapper>
-    </HexGreenBGWrapper> */}
-    <Button value={'Testing'} additionalClasses={'border-8, bg-amber-400'}/>
-    </>
-  );
+    <div className=''>
+      <button className='bg-jade hover:bg-kelly-green text-white font-bold py-2 px-4 m-2 rounded shadow-lg '>Primary</button>
+      <button className='bg-myrtle-green hover:bg-sea-green text-white font-bold py-2 px-4 m-2 rounded shadow-cyan-lg'>Secondary</button>
+    </div>
+  )
 }
 
 export default Prototype;
