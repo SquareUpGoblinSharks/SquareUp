456d86838092b22ccc1869be944e6ebf15df5763
Dana Kaplan <danakaplan96@gmail.com>
2023-11-09 17:20:09 -0500

    styling for matches

--- client/components/UpcomingMatches.jsx
+++ client/components/UpcomingMatches.jsx
@@ -8,8 +8,8 @@ const UpcomingMatches = () => {
     console.log("hello from forEach", element.name);
     return (
       <div className={"flex justify-between"} key={crypto.randomUUID()}>
-        <div>{element.name}</div>
-        <div>
+        <div className="font-medium text-white">{element.name}</div>
+        <div className="font-medium text-white">
           {element.totalWins}/{element.totalLosses}
         </div>
       </div>
@@ -19,11 +19,13 @@ const UpcomingMatches = () => {
   return (
     <div className="bg-dark-slate-green m-5 p-5 border-2 border-gray-700 overflow-scroll h-[90vh] rounded-lg">
       <div className="upcomingMatches">
-        <h2>UPCOMING MATCHES</h2>
+        <h2 className="text-xl font-bold text-white bg-dark-slate-green p-2 px-4 sticky top-0 z-10">
+          UPCOMING MATCHES
+        </h2>
         <div id="wrapper" className={"flex flex-col"}>
           <div id="header-wrapper" className={"flex justify-between"}>
-            <div>Name</div>
-            <div>W/L</div>
+            <div className="font-extrabold text-white">Name</div>
+            <div className="font-extrabold text-white">W/L</div>
           </div>
           <div id="matches" className={"flex flex-col"}>
             {matchElems}
