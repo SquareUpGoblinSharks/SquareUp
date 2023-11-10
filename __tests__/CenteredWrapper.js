4417691b9b1e090fbd6c5c7c1e0af4c7f7f67cbb
Evan Pearson <parsnbl@icloud.com>
2023-11-09 16:56:45 -0800

    wrappers and upcoming matches tests

---
+++ __tests__/CenteredWrapper.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import regeneratorRuntime from 'regenerator-runtime';
+
+import '@testing-library/jest-dom';
+
+
+
+import CenteredWrapper from '../client/components/CenteredWrapper';
+
+
+describe('CenteredWrapper unit tests', ()=>{
+  let wrapper;
+  beforeEach(()=>{
+    wrapper = render(
+    <CenteredWrapper>
+      <div>TEST CHILD</div>  
+      <div>TEST CHILD</div>  
+      <div>TEST CHILD</div>  
+      <div>TEST CHILD</div>  
+      <div>TEST CHILD</div>  
+    </CenteredWrapper>
+    );
+  });
+  test('should have default flex settings', () => {
+    expect(screen.getByTestId('centered-wrapper')).toHaveClass('flex');
+    expect(screen.getByTestId('centered-wrapper')).toHaveClass('items-center');
+    expect(screen.getByTestId('centered-wrapper')).toHaveClass('justify-center');
+    expect(screen.getByTestId('centered-wrapper')).toHaveClass('flex-col');
+    expect(screen.getByTestId('centered-wrapper')).toHaveClass('h-full');
+    expect(screen.getByTestId('centered-wrapper')).toHaveClass('w-full');
+    expect(screen.getByTestId('centered-wrapper')).toHaveClass('bg-inherit');
+  })
+  test('should render all children correctly', () => {
+    expect(wrapper.getAllByText('TEST CHILD').length).toBe(5);
+  })
+});
